eff6dc68ff92adde17064350b0b733e23c77aa6a
Victor Mao <victormao98@gmail.com>
2017-03-29 00:48:06 -0500

    initial commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn forage4food.wsgi
---
+++ forage4food/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for forage4food project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.10/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "forage4food.settings")
+
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
